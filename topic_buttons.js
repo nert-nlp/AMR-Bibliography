a732721c794a1ed54d2cd1f558c05b8f7b44bfb3
ablodge <ablodge@users.noreply.github.com>
2018-10-30 18:33:10 -0400

    bug fix

--- topic_buttons.js
+++ topic_buttons.js
@@ -10,16 +10,17 @@ $(document).ready(function () {
     $("button").on({
         click:function(){
             if ($(this).attr('on')==='0'){
+                $(this).attr('on','1');
                 let c = $(this).attr('topic');
                 $(`button[topic='${c}']`).addClass('green');
                 $(`button[topic='${c}']`).closest('tr').addClass('green');
-                $(this).attr('on','1');
             }
             else {
                 let c = $(this).attr('topic');
+                $(this).attr('on','0');
                 $(`button[topic='${c}']`).removeClass('green');
                 $(`button[topic='${c}']`).closest('tr').removeClass('green');
-                $(this).attr('on','0');
+                $(`button[on='1']`).closest('tr').addClass('green');
             }
         }
     });
